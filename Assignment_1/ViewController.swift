add35fc93b7c0b2f91d483ac041c410a6a8312b3
Vinh Tran <s3500659@rmit.edu.au>
2020-09-07 10:29:11 +1000

    Initial Commit

---
+++ Assignment_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment_1
+//
+//  Created by Vinh Tran on 7/9/20.
+//  Copyright © 2020 Vinh Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Assignment_1Tests/Assignment_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Assignment_1Tests.swift
+//  Assignment_1Tests
+//
+//  Created by Vinh Tran on 7/9/20.
+//  Copyright © 2020 Vinh Tran. All rights reserved.
+//
+
+import XCTest
+@testable import Assignment_1
+
+class Assignment_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
