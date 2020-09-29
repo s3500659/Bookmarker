bd8f871b180ba2bbb8e3318d5721d55f0b9f563a
Vinh Tran <s3500659@student.rmit.edu.au>
2020-09-29 16:45:19 +1000

    complete student info master / detail view

--- Assignment_1/Controllers/StudentInfoTableViewController.swift
+++ Assignment_1/Controllers/StudentInfoTableViewController.swift
@@ -131,8 +131,12 @@ class StudentInfoTableViewController: UITableViewController {
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        guard let selectedRow = self.tableView.indexPathForSelectedRow else {return}
+        
+        let destination = segue.destination as? StudentInfoDetailViewController
+        let selectedStudent = students[selectedRow.row]
+        destination?.selectedStudent = selectedStudent
     }
 
 
