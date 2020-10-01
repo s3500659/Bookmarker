bee4915f3c8fcf7740e38b8af2c35fe2ab0805bf
Vinh Tran <s3500659@student.rmit.edu.au>
2020-10-01 13:30:52 +1000

    now able to search books by title

--- Assignment_1/Controllers/SearchBookTableViewController.swift
+++ Assignment_1/Controllers/SearchBookTableViewController.swift
@@ -8,15 +8,31 @@
 
 import UIKit
 
-class SearchBookTableViewController: UITableViewController {
-    
+class SearchBookTableViewController: UITableViewController ,UISearchBarDelegate {
     
     // 1 - need connection to the ViewModel
     
-    private let viewModel = SearchBooksViewModel()
+    var viewModel = SearchBooksViewModel()
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    var data :[String] = []
+    
+    var filteredData: [String]!
+    
+    func loadBookData() {
+        for i in viewModel.books {
+            data.append(i.title)
+        }
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadBookData()
+        tableView.dataSource = self
+        searchBar.delegate = self
+        filteredData = data
         
     }
     
@@ -25,27 +41,44 @@ class SearchBookTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #counts the number of books in the BookData file
-        return viewModel.count
+        return filteredData.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
         // change this
-        let imageView = cell.viewWithTag(1000) as? UIImageView
-        let bookTitle = cell.viewWithTag(1001) as? UILabel
-        let bookAuthor = cell.viewWithTag(1002) as? UILabel
-        // creates local variables of same vars as above
-        if let imageView = imageView, let bookTitle = bookTitle, let bookAuthor = bookAuthor
-        {
-            let currentBook = viewModel.getBooks(byIndex: indexPath.row)
-            imageView.image = currentBook.image
-            bookTitle.text = currentBook.title
-            bookAuthor.text = currentBook.author
-        }
+//        let imageView = cell.viewWithTag(1000) as? UIImageView
+//        let bookTitle = cell.viewWithTag(1001) as? UILabel
+//        let bookAuthor = cell.viewWithTag(1002) as? UILabel
+//        // creates local variables of same vars as above
+//        if let imageView = imageView, let bookTitle = bookTitle, let bookAuthor = bookAuthor
+//        {
+//            let currentBook = viewModel.getBooks(byIndex: indexPath.row)
+//            imageView.image = currentBook.image
+//            bookTitle.text = currentBook.title
+//            bookAuthor.text = currentBook.author
+//        }
+        cell.textLabel?.text = filteredData[indexPath.row]
         
         return cell
     }
+    
+    /// This method updates filteredData based on the text in the Search Box
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        filteredData = searchText.isEmpty ? data : data.filter({(dataString: String) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return dataString.range(of: searchText, options: .caseInsensitive) != nil
+        })
+        
+        tableView.reloadData()
+    }
+
     
     // MARK: - Navigation
     
--- Assignment_1/Models/BookData.swift
+++ Assignment_1/Models/BookData.swift
@@ -8,10 +8,23 @@
 
 import Foundation
 
-enum BookData: String {
+enum BookData: String, CaseIterable {
     
-    case Book1 = "Gone with the Wind ", Book2 = "Essays", Book3 = "Cloud Computing", Book4 = "Introduction to Java Programming", Book5 = "Lord of the Flies",
-    Book6 = "Catch 22", Book7 = "Middlemarch", Book8 = "The Time Machine"
+    case Book1, Book2, Book3, Book4, Book5,
+    Book6, Book7, Book8
+    
+    var title:String {
+        switch self {
+        case .Book1: return "Gone with the Wind"
+        case .Book2: return "Essays"
+        case .Book3: return "Cloud Computing"
+        case .Book4: return "Introduction to Java Programming"
+        case .Book5: return "Lord of the Flies"
+        case .Book6: return "Catch 22"
+        case .Book7: return "middle"
+        case .Book8: return "The Time Machine"
+        }
+    }
     
     var imageName: String{
         switch self {
