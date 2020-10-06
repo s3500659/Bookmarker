4c97e4db5b701477da11f211215d0de2bf69649c
3612419 <37232687+3612419@users.noreply.github.com>
2020-10-07 04:48:22 +1000

    initial setup

---
+++ Assignment_1/Controllers/ProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Assignment_1
+//
+//  Created by admin on 7/10/20.
+//  Copyright © 2020 Vinh Tran. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
