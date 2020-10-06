e84b680549d8753e085aaa9491505ae78a16e96d
3612419 <37232687+3612419@users.noreply.github.com>
2020-10-07 09:00:50 +1000

    updated view controller

--- Assignment_1/Controllers/ProfileViewController.swift
+++ Assignment_1/Controllers/ProfileViewController.swift
@@ -1,25 +1,58 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  Assignment_1
 //
-//  Created by admin on 7/10/20.
+//  Created by Josh on 7/10/20.
 //  Copyright © 2020 Vinh Tran. All rights reserved.
 //
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bookData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "favouriteBookCell", for: indexPath) as? UITableViewCell else {
+            fatalError("The dequeued cell with identifier 'favouriteBookCell' is invalid")
+        }
+        cell.textLabel?.text=bookData[indexPath.row]
+        return cell
+    }
+    
 
+    
+    @IBOutlet weak var yourProfile: UILabel!
+    
+    @IBOutlet weak var booksFinished: UILabel!
+    
+    @IBOutlet weak var booksFinishedCount: UILabel!
+    
+    @IBOutlet weak var favouriteBooks: UILabel!
+    
+    @IBOutlet weak var favouriteBooksTable: UITableView!
+    
+    var bookData:[String] = []
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        favouriteBooksTable.delegate=self
+        favouriteBooksTable.dataSource=self
+        
+        
+        //temp dummy data
+        for index in 0...20{
+            bookData.append(String(index))
+        }
+        booksFinishedCount.text=String(bookData.count)
     }
     
     
     
 
-    
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -27,6 +60,5 @@ class ProfileViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    
 
 }
