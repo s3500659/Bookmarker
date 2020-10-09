ebe45b53f57838311a38c9b58254a997e80874e4
Philip Beeby <pbeeby@mac.com>
2020-10-09 18:40:22 +1100

    adding a logo for the app and adding pics to student profile section

--- Assignment_1/Controllers/ViewModels/StudentViewModel.swift
+++ Assignment_1/Controllers/ViewModels/StudentViewModel.swift
@@ -22,7 +22,7 @@ struct StudentViewModel {
             name: "Vinh",
             studentNumber: "s3500659",
             description: "Currently a student studying a Bachelor of Information Technology at RMIT university. I enjoy learning the various concepts and technology taught in this course especially the programming side of thing even when it's difficult",
-            image: UIImage(named: "lion_king_1")!,
+            image: UIImage(named: "h_g_wells")!,
             hobbies: "Brazillian jiu-jitsu, Video games, Food, Snowboarding",
             additionalInfo: "In my spare time I like to play video games, train BJJ, kickboxing, wrestling, travelling, going out to eat. I also love to snowboard so every winter you will find me and my friends hanging around Perisher sliding up and down the mountain.")
         
@@ -30,7 +30,7 @@ struct StudentViewModel {
             name: "Josh",
             studentNumber: "s3612419",
             description: "I began my journey into IOS app development in late August and have been hooked ever since. Helping to create BookMarker was an especially exciting task since it combined my love of reading and enthusiasm for app development.",
-            image: UIImage(named: "lion_king_2")!,
+            image: UIImage(named: "dan_abnet")!,
             hobbies: "Photography, Hiking, Programming, Cycling, Reading.",
             additionalInfo: "")
         
@@ -38,7 +38,7 @@ struct StudentViewModel {
             name: "Philip",
             studentNumber: "s3666628",
             description: "I wanted to learn to write code after working in IT for many years but not able to understand how computer programs actually work. After studying part-time with RMIT since 2017, some of the mysteries have been uncovered but I realised that there is so much more I still don't understand.",
-            image: UIImage(named: "lion_king_3")!,
+            image: UIImage(named: "george_orwell")!,
             hobbies: "Running, reading, horror movie affionado, drinking cups of tea, playing video games, talking politics and philosophy.",
             additionalInfo: "Favourite place to visit:\nI recently went to Japan and was blown away by the culture and food and the amazingly efficient trains system. I also love to go back to the UK to catch up with my family and friends. I was back there in Feb where I had some old-fashioned English beer in the oldest pub in England.")
         
@@ -46,7 +46,7 @@ struct StudentViewModel {
             name: "Jordan",
             studentNumber: "s3611566",
             description: "I currently work full time as a web developer using Ruby on Rails while completing my final unit for my Bachelor of IT. I enjoy solving complex programming problems.",
-            image: UIImage(named: "simba_1")!,
+            image: UIImage(named: "f_scott_fitzgerald")!,
             hobbies: "Video games, boxing, web development and fishing",
             additionalInfo: "In my spare time I am trying to find the best fishing spots, playing video games or working on hobby development projects.")
         
