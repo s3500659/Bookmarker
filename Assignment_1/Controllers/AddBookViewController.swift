219f1e2d6fff15eafeadd9bcdc48d908f3f01d5b
3612419 <37232687+3612419@users.noreply.github.com>
2020-10-11 20:07:16 +1000

    removed more commented code

--- Assignment_1/Controllers/AddBookViewController.swift
+++ Assignment_1/Controllers/AddBookViewController.swift
@@ -111,14 +111,5 @@ class AddBookViewController: UIViewController,UITableViewDelegate,UITableViewDat
         self.preferredContentSize = self.view.systemLayoutSizeFitting(
             UIView.layoutFittingCompressedSize)
     }
-    
-    
-    
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
+
 }
--- Assignment_1/Controllers/ProfilePopOverViewController.swift
+++ Assignment_1/Controllers/ProfilePopOverViewController.swift
@@ -111,16 +111,6 @@ class ProfilePopOverViewController: UIViewController,UITableViewDelegate,UITable
             UIView.layoutFittingCompressedSize)
         
     }
-    
-    
-
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
 }
 
 
