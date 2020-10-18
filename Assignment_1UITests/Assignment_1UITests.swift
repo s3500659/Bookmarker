c2dc73224eaa1793ae40323c0e2290128f7e5347
Philip Beeby <pbeeby@mac.com>
2020-10-18 15:19:20 +1100

    added UI tests for the tab bars

--- Assignment_1UITests/Assignment_1UITests.swift
+++ Assignment_1UITests/Assignment_1UITests.swift
@@ -11,6 +11,7 @@ import XCTest
 class Assignment_1UITests: XCTestCase {
 
     override func setUp() {
+        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -24,11 +25,63 @@ class Assignment_1UITests: XCTestCase {
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testTabBarNamesSearch() {
+        // test the labels on the Tab Bars are named correctly
+        let expectedTabName = "Search Books"
+        let tabBarsQuery = XCUIApplication().tabBars
+        let testTabBarItem = tabBarsQuery.buttons[expectedTabName].label
+        XCTAssertEqual(testTabBarItem, expectedTabName, "Tab Bar Expected Name: \(expectedTabName), Actual Name is \(testTabBarItem)")
+
     }
+    
+    func testTabBarNamesmyBooks() {
+        // test the labels on the Tab Bars are named correctly
+        let expectedTabName = "My Books"
+        let tabBarsQuery = XCUIApplication().tabBars
+        let testTabBarItem = tabBarsQuery.buttons[expectedTabName].label
+        XCTAssertEqual(testTabBarItem, expectedTabName, "Tab Bar Expected Name: \(expectedTabName), Actual Name is \(testTabBarItem)")
+        
+    }
+    
+    func testTabBarNamesProfile() {
+        // test the labels on the Tab Bars are named correctly
+        let expectedTabName = "Profile"
+        let tabBarsQuery = XCUIApplication().tabBars
+        let testTabBarItem = tabBarsQuery.buttons[expectedTabName].label
+        XCTAssertEqual(testTabBarItem, expectedTabName, "Tab Bar Expected Name: \(expectedTabName), Actual Name is \(testTabBarItem)")
+        
+    }
+    
+    func testTabBarNamesStudent() {
+        // test the labels on the Tab Bars are named correctly
+        let expectedTabName = "Students"
+        let tabBarsQuery = XCUIApplication().tabBars
+        let testTabBarItem = tabBarsQuery.buttons[expectedTabName].label
+        XCTAssertEqual(testTabBarItem, expectedTabName, "Tab Bar Expected Name: \(expectedTabName), Actual Name is \(testTabBarItem)")
+        
+    }
+    
+    
+    
+    func testTabBarCount() {
+        // test there is one tab bar and only one tab bar
+        let expectedTabCount: Int = 1
+        let tabBarsQuerycount = XCUIApplication().tabBars.count
+        XCTAssertEqual(tabBarsQuerycount, expectedTabCount, "number of tab bar items is incorrect Expected Number: \(expectedTabCount) Actual Number is \(tabBarsQuerycount)")
+
+    }
+//    func testRecord() {
+//
+//
+//        let tabBarsQuery = XCUIApplication().tabBars
+//        tabBarsQuery.buttons["Search Books"].tap()
+//        tabBarsQuery.buttons["Profile"].tap()
+//        tabBarsQuery.buttons["Students"].tap()
+//
+//
+//    }
 
 }
