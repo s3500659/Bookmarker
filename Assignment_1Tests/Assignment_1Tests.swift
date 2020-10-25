c5e78109a96955ed77fe2b7d640a987aa68a1179
Philip Beeby <pbeeby@mac.com>
2020-10-26 09:27:55 +1100

    starting on adding the unit tests

--- Assignment_1Tests/Assignment_1Tests.swift
+++ Assignment_1Tests/Assignment_1Tests.swift
@@ -10,20 +10,41 @@ import XCTest
 @testable import Assignment_1
 
 class Assignment_1Tests: XCTestCase {
+    
+//    let testImage = UIImage(named: "h_g_wells")!
+    var testStudent: Student = Student(name: "testStudentName", fullName: "full name", studentNumber: "s123456", description: "test desciption", image: UIImage(named: "h_g_wells")!, hobbies: "testing hobby", additionalInfo: "I like making unit tests")
+   
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        // init(name:String, fullName:String, studentNumber:String, description:String, image:UIImage, hobbies:String, additionalInfo:String)
+
+//        testStudent = Student(name: "testStudentName", fullName: "full name", studentNumber: "s123456", description: "test desciption", image: UIImage(named: "h_g_wells")!, hobbies: "testing hobby", additionalInfo: "I like making unit tests")
+       
+//        testStudent = Student(name: "testStudentName", fullName: "full name", studentNumber: "s123456", description: "test desciption", image: testImage, hobbies: "testing hobby", additionalInfo: "I like making unit tests")
+//        var studentName = testStudent.name
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testValidStudentName() {
+        //test that Student object can be created with a first name
+        let expectedName: String = "testStudentName"
+        XCTAssertNotNil(testStudent.name)
+        XCTAssertTrue(testStudent.name == expectedName, "Student name is not working correctly, expected: \(expectedName) is not the same as actual name: \(testStudent.name)")
+        
     }
-
+    
+    func testValidStudentFullName() {
+        //test that Student object can be created with a first name
+        let expectedFullName: String = "full name"
+        XCTAssertNotNil(testStudent.fullName)
+        XCTAssertTrue(testStudent.fullName == expectedFullName, "Student fullname is not working correctly, expected: \(expectedFullName) is not the same as actual name: \(testStudent.fullName)")
+        
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
